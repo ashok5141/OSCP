70eec383a66366fb91317892152046da29f8c503
Ashok <134892274+ashok5141@users.noreply.github.com>
2024-08-11 10:59:25 -0500

    Create WindowsSearchTXT.ps1
    It search all the Txt files and put the content into one txt file

---
+++ ActiveDirectoryScripts/WindowsSearchTXT.ps1
@@ -0,0 +1,18 @@
+# Get all .txt files in C:\<Choose the path> and create Output file its subdirectories
+$txtFiles = Get-ChildItem -Path C:\Users\Ashok\Desktop -Filter *.txt -Recurse
+
+# Create an output file where you want
+$outputFile = "C:\Users\Ashok\Desktop\output.txt"
+
+# Loop through each .txt file and view its contents using the Type command
+foreach ($file in $txtFiles) {
+    # Write the file path to the output file
+    Add-Content -Path $outputFile -Value "------------------------"
+    Add-Content -Path $outputFile -Value $file.FullName
+    Add-Content -Path $outputFile -Value "------------------------"
+    
+    # Write the file contents to the output file
+    Get-Content -Path $file.FullName | Add-Content -Path $outputFile
+}
+
+Write-Host "Output saved to $outputFile"
