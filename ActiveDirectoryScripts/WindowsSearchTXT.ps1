d43d87861bbfbe2abf76d11945bc6462b9a5fc1e
Ashok <134892274+ashok5141@users.noreply.github.com>
2024-08-11 11:04:27 -0500

    Update WindowsSearchTXT.ps1
    Added create file command

--- ActiveDirectoryScripts/WindowsSearchTXT.ps1
+++ ActiveDirectoryScripts/WindowsSearchTXT.ps1
@@ -2,6 +2,8 @@
 $txtFiles = Get-ChildItem -Path C:\Users\Ashok\Desktop -Filter *.txt -Recurse
 
 # Create an output file where you want
+#Powershell
+#New-Item -Path C:\Users\username\Documents\emptyfile.txt -ItemType File
 $outputFile = "C:\Users\Ashok\Desktop\output.txt"
 
 # Loop through each .txt file and view its contents using the Type command
